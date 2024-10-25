bfd8033cad028ad3c5502617f37dd2e5ac6c7314
Aayush <aayushkafle308@gmail.com>
2024-10-24 20:38:49 -0600

    Fixed the error code

--- api/User.js
+++ api/User.js
@@ -97,7 +97,7 @@ router.post('/signup', (req, res) => {
 })
 
 // Login route
-router.get('/signin', (req, res) => {
+router.post('/signin', (req, res) => {
     let {email, password} = req.body;
     email = email.trim();
     password = password.trim();
